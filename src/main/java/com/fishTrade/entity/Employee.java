8dc652fba6ce8f6765a103465bfd7aee64567626
Kris <krisgodom@gmail.com>
2021-03-26 16:46:51 -0300

    created POJO Employee

--- src/main/java/com/fishTrade/entity/Employee.java
+++ src/main/java/com/fishTrade/entity/Employee.java
@@ -1,2 +1,42 @@
-package com.fishTrade.entity;public class Employee {
+package com.fishTrade.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private Position position;
+
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
