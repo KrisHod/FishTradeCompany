ffe06748aaf99509ca6a84114dbbadff966005b7
Kris <krisgodom@gmail.com>
2021-03-26 16:47:22 -0300

    created POJO Fish

--- src/main/java/com/fishTrade/entity/Fish.java
+++ src/main/java/com/fishTrade/entity/Fish.java
@@ -1,2 +1,78 @@
-package com.fishTrade.entity;public class Fish {
+package com.fishTrade.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double weight;
+    private double price;
+    private String description;
+    private boolean isRegistered;
+    private boolean isWrittenOff;
+
+    public Fish() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRegistered() {
+        return isRegistered;
+    }
+
+    public void setRegistered(boolean registered) {
+        isRegistered = registered;
+    }
+
+    public boolean isWrittenOff() {
+        return isWrittenOff;
+    }
+
+    public void setWrittenOff(boolean writtenOff) {
+        isWrittenOff = writtenOff;
+    }
 }
