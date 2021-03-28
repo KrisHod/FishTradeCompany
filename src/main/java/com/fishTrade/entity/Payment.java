ff8e2d27b273b25165f021e988b15f364318b0aa
Kris <krisgodom@gmail.com>
2021-03-28 09:53:51 -0300

    created POJO Payment

--- src/main/java/com/fishTrade/entity/Payment.java
+++ src/main/java/com/fishTrade/entity/Payment.java
@@ -1,2 +1,27 @@
-package com.fishTrade.entity;public class Payment {
+package com.fishTrade.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private double sum;
+    private java.sql.Timestamp dateTime;
+
+    private Order order;
+    private Employee employee;
+
+    public Payment(int id, double sum, Timestamp dateTime, Order order, Employee employee) {
+        this.id = id;
+        this.sum = sum;
+        this.dateTime = dateTime;
+        this.order = order;
+        this.employee = employee;
+    }
 }
