f3766b8a9eae94e5c1d4d04e04102626f140c243
Kris <krisgodom@gmail.com>
2021-03-28 09:52:30 -0300

    created POJO Order

--- src/main/java/com/fishTrade/entity/Order.java
+++ src/main/java/com/fishTrade/entity/Order.java
@@ -1,2 +1,54 @@
-package com.fishTrade.entity;public class Order {
+package com.fishTrade.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private java.sql.Date ordered;
+    private java.sql.Date shipped;
+    private OrderStatus orderStatus;
+    private Customer customer;
+
+    public Order() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getOrdered() {
+        return ordered;
+    }
+
+    public void setOrdered(Date ordered) {
+        this.ordered = ordered;
+    }
+
+    public Date getShipped() {
+        return shipped;
+    }
+
+    public void setShipped(Date shipped) {
+        this.shipped = shipped;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 }
