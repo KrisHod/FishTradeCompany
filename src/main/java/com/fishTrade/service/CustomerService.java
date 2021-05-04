aaceae2dda4dec64ee59c2685992b1ee94dbdab3
Kris <krisgodom@gmail.com>
2021-05-04 12:19:54 -0300

    created basic methods

--- src/main/java/com/fishTrade/service/CustomerService.java
+++ src/main/java/com/fishTrade/service/CustomerService.java
@@ -1,2 +1,24 @@
-package com.fishTrade.service;public class CustomerService {
+package com.fishTrade.service;
+
+import com.fishTrade.entity.Customer;
+import com.fishTrade.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public class CustomerService implements ICustomerService{
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Override
+    public Optional<Customer> findById (long id){
+        return customerRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public void save (Customer cus){
+        customerRepository.save(cus);
+    }
 }
