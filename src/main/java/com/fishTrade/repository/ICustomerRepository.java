34a00bf6dcca77dedc84cca615a28f7c0e423e99
Kris <krisgodom@gmail.com>
2021-05-04 12:18:43 -0300

    created basic methods

---
+++ src/main/java/com/fishTrade/repository/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.fishTrade.repository;
+
+import com.fishTrade.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
