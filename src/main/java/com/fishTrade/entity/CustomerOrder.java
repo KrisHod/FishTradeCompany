9b43578c582ca0c995bb820415e286918f8b12ec
Kris <krisgodom@gmail.com>
2021-05-07 19:03:16 -0300

    updated POJO CustomerOrder

--- src/main/java/com/fishTrade/entity/CustomerOrder.java
+++ src/main/java/com/fishTrade/entity/CustomerOrder.java
@@ -3,22 +3,32 @@ package com.fishTrade.entity;
 import javax.persistence.*;
 import java.sql.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
-public class Order {
+public class CustomerOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private java.sql.Date ordered;
     private java.sql.Date shipped;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
     @ManyToOne
-    @JoinColumn(name = "customerId")
+    @JoinColumn(name ="customerId")
     private Customer customer;
 
-List<FishParcel> fishParcelList;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "ordered_fish_parcel", joinColumns = @JoinColumn(name = "orderId"),
+            inverseJoinColumns = @JoinColumn(name = "fishParcelId"))
+    List<FishParcel> fishParcelList;
 
-    public Order() {
+    @OneToMany(mappedBy = "customerOrder")
+    private List<Payment> payments;
+
+    public CustomerOrder() {
 
     }
 
@@ -52,5 +62,55 @@ List<FishParcel> fishParcelList;
 
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<FishParcel> getFishParcelList() {
+        return fishParcelList;
+    }
+
+    public void setFishParcelList(List<FishParcel> fishParcelList) {
+        this.fishParcelList = fishParcelList;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomerOrder that = (CustomerOrder) o;
+        return id == that.id && Objects.equals(ordered, that.ordered) && Objects.equals(shipped, that.shipped) && orderStatus == that.orderStatus && Objects.equals(customer, that.customer) && Objects.equals(fishParcelList, that.fishParcelList) && Objects.equals(payments, that.payments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ordered, shipped, orderStatus, customer, fishParcelList, payments);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerOrder{" +
+                "id=" + id +
+                ", ordered=" + ordered +
+                ", shipped=" + shipped +
+                ", orderStatus=" + orderStatus +
+                ", customer=" + customer +
+                ", fishParcelList=" + fishParcelList +
+                ", payments=" + payments +
+                '}';
     }
 }
