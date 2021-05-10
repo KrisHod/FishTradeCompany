d5112ff80d0168fdab48f28e0a3ed763e6a3e06b
Kris <krisgodom@gmail.com>
2021-05-10 13:31:47 -0300

    updated Position

--- src/main/java/com/fishTrade/entity/Position.java
+++ src/main/java/com/fishTrade/entity/Position.java
@@ -1,5 +1,58 @@
 package com.fishTrade.entity;
 
-public enum Position {
-    SECURITY_OFFICER, GENERAL_MANAGER, COLD_STORE_MANAGER, ACCOUNTANT;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    //SECURITY_OFFICER, GENERAL_MANAGER, COLD_STORE_MANAGER, ACCOUNTANT;
+
+
+    public Position() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return id == position.id && Objects.equals(name, position.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
