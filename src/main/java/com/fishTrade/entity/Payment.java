df80fb5f8aaa33243789a105aae3d74662a681c0
Kris <krisgodom@gmail.com>
2021-05-10 13:32:23 -0300

    updated POJO Payment

--- src/main/java/com/fishTrade/entity/Payment.java
+++ src/main/java/com/fishTrade/entity/Payment.java
@@ -1,10 +1,8 @@
 package com.fishTrade.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 public class Payment {
@@ -14,14 +12,78 @@ public class Payment {
     private double sum;
     private java.sql.Timestamp dateTime;
 
-    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "orderId")
+    private CustomerOrder customerOrder;
+
+    @ManyToOne
+    @JoinColumn(name = "employeeId")
     private Employee employee;
 
-    public Payment(int id, double sum, Timestamp dateTime, Order order, Employee employee) {
+    public Payment() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
         this.sum = sum;
+    }
+
+    public Timestamp getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Timestamp dateTime) {
         this.dateTime = dateTime;
-        this.order = order;
+    }
+
+    public CustomerOrder getCustomerOrder() {
+        return customerOrder;
+    }
+
+    public void setCustomerOrder(CustomerOrder customerOrder) {
+        this.customerOrder = customerOrder;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
         this.employee = employee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return id == payment.id && Double.compare(payment.sum, sum) == 0 && Objects.equals(dateTime, payment.dateTime) && Objects.equals(customerOrder, payment.customerOrder) && Objects.equals(employee, payment.employee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, sum, dateTime, customerOrder, employee);
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + id +
+                ", sum=" + sum +
+                ", dateTime=" + dateTime +
+                ", customerOrder=" + customerOrder +
+                ", employee=" + employee +
+                '}';
     }
 }
