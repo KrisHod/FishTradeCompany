bccb6d0c7597327bf039fb03a4448439eace0ab8
Kris <krisgodom@gmail.com>
2021-05-12 09:52:20 -0300

    updated IFishService

--- src/main/java/com/fishTrade/service/IFishService.java
+++ src/main/java/com/fishTrade/service/IFishService.java
@@ -1,2 +1,11 @@
-package com.fishTrade.service;public interface IFishService {
+package com.fishTrade.service;
+
+import com.fishTrade.entity.Fish;
+
+import java.util.Optional;
+
+public interface IFishService {
+    Optional<Fish> findById(long id);
+
+    void save(Fish fish);
 }
