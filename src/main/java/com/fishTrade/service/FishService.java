757cd3bc20930bf813bd941a0a66df7938c33c3a
Kris <krisgodom@gmail.com>
2021-05-12 10:01:20 -0300

    updated FishService

--- src/main/java/com/fishTrade/service/FishService.java
+++ src/main/java/com/fishTrade/service/FishService.java
@@ -1,2 +1,25 @@
-package com.fishTrade.service;public class FishService {
+package com.fishTrade.service;
+
+import com.fishTrade.entity.Fish;
+import com.fishTrade.repository.IFishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+
+public class FishService implements IFishService{
+    @Autowired
+    private IFishRepository fishRepository;
+
+    @Override
+    public Optional<Fish> findById (long id){
+        return fishRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public void save (Fish fish){
+        fishRepository.save(fish);
+    }
 }
