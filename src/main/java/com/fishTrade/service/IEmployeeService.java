da9bd644b24885feb945d0562ad39532914f3812
Kris <krisgodom@gmail.com>
2021-05-12 09:51:42 -0300

    updated IEmployeeService

--- src/main/java/com/fishTrade/service/IEmployeeService.java
+++ src/main/java/com/fishTrade/service/IEmployeeService.java
@@ -1,2 +1,11 @@
-package com.fishTrade.service;public interface IEmployeeService {
+package com.fishTrade.service;
+
+import com.fishTrade.entity.Employee;
+
+import java.util.Optional;
+
+public interface IEmployeeService {
+    Optional<Employee> findById(long id);
+
+    void save(Employee employee);
 }
