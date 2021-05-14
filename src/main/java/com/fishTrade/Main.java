e50e3135e75f61f08dc990b8647bfbbe52d321ec
Kris <krisgodom@gmail.com>
2021-05-14 08:54:56 -0300

    checked functionality

--- src/main/java/com/fishTrade/Main.java
+++ src/main/java/com/fishTrade/Main.java
@@ -1,2 +1,14 @@
-package com.fishTrade;public class Main {
+package com.fishTrade;
+
+import com.fishTrade.service.ICustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ICustomerService customerService = context.getBean(ICustomerService.class);
+        customerService.findById(1).ifPresent(System.out::println);
+    }
 }
