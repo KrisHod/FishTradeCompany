1eac69b60f4dbab38a14418d2afb57e67c14bcb7
Kris <krisgodom@gmail.com>
2021-05-15 16:38:42 -0300

    implemented methods

--- src/main/java/com/fishTrade/service/CustomerOrderService.java
+++ src/main/java/com/fishTrade/service/CustomerOrderService.java
@@ -1,2 +1,26 @@
-package com.fishTrade.service;public class CustomerOrderService {
+package com.fishTrade.service;
+
+import com.fishTrade.entity.CustomerOrder;
+import com.fishTrade.repository.ICustomerOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class CustomerOrderService implements ICustomerOrderService{
+    @Autowired
+    private ICustomerOrderRepository customerOrderRepository;
+
+    @Override
+    public Optional<CustomerOrder> findById (long id){
+        return customerOrderRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public void save (CustomerOrder order){
+        customerOrderRepository.save(order);
+    }
 }
