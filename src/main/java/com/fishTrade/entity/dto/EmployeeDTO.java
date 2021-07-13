aa844e03b3e7775eb476f3a192c9dadcd6f21e65
Kris <krisgodom@gmail.com>
2021-07-13 10:19:02 -0300

    add EmployeeDTO

--- src/main/java/com/fishTrade/entity/dto/EmployeeDTO.java
+++ src/main/java/com/fishTrade/entity/dto/EmployeeDTO.java
@@ -1,2 +1,15 @@
-package com.fishTrade.entity.dto;public class EmployeeDTO {
+package com.fishTrade.entity.dto;
+
+import com.fishTrade.entity.Position;
+import lombok.Data;
+
+
+@Data
+public class EmployeeDTO {
+    private int id;
+    private String name;
+    private Position position;
+
+    public EmployeeDTO() {
+    }
 }
