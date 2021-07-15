1f8688a55d6c30957249556ddbb301c5a551a973
Kris <krisgodom@gmail.com>
2021-07-15 11:16:25 -0300

    add CRUD operations to EmployeeService

--- src/main/java/com/fishTrade/service/EmployeeService.java
+++ src/main/java/com/fishTrade/service/EmployeeService.java
@@ -1,25 +1,58 @@
 package com.fishTrade.service;
 
 import com.fishTrade.entity.Employee;
+import com.fishTrade.entity.Position;
+import com.fishTrade.exception.ResourceNotFoundException;
 import com.fishTrade.repository.IEmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
-public class EmployeeService implements IEmployeeService{
-    @Autowired
-    private IEmployeeRepository employeeRepository;
+@AllArgsConstructor
+public class EmployeeService implements IEmployeeService {
+    private final IEmployeeRepository employeeRepository;
 
     @Override
-    public Optional<Employee> findById (long id){
-        return employeeRepository.findById(id);
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
     }
 
-    @Transactional
     @Override
-    public void save (Employee employee){
+    public Employee findByPosition(Position position) {
+        return employeeRepository.findByPosition(position);
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer with id " + id + " was not found"));
+    }
+
+    @Override
+    public Employee updateEmployee(int id, Employee employeeDetails) {
+        Employee employee = findById(id);
+        employee.setName(employeeDetails.getName());
+        employee.setPosition(employeeDetails.getPosition());
+        save(employee);
+        return employee;
+    }
+
+    @Override
+    public Map<String, Boolean> deleteEmployee(int id) {
+        Map<String, Boolean> response = new HashMap<>();
+        Employee employee = findById(id);
+        employeeRepository.delete(employee);
+        response.put("Employee", Boolean.TRUE);
+        return response;
+    }
+
+    @Override
+    public void save(Employee employee) {
         employeeRepository.save(employee);
     }
 }
